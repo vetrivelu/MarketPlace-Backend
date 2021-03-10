0da6b728854d37a5ccd2db9c7333f09e59570412
Akon <79909691+MeenakshiSundaram07@users.noreply.github.com>
2021-03-10 18:48:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet    =   require('helmet');
-const CartLength = require('./Middleware/Cart');
+const CartLength = require('./Midddleware/Cart');
 
 //route imports
 
